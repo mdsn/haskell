443a3db33545345ee7dca0b3a36046292b6c44f9
Mariano Casco <muy.poca.fe@gmail.com>
2013-04-17 13:52:24 -0300

    concat, selections

--- lists.hs
+++ lists.hs
@@ -6,3 +6,14 @@ intersperse x (y:ys) = y:x:intersperse x ys
 intercalate :: [a] -> [[a]] -> [a]
 intercalate x (y:[]) = y
 intercalate x (y:ys) = y ++ x ++ intercalate x ys
+
+-- ??
+--transpose :: [[a]] -> [[a]]
+
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (x:xs) = x ++ concat' xs
+
+selections :: [a] -> [(a,[a])]
+selections [] = []
+selections (x:xs) = (x,xs) : [ (y,x:ys) | (y,ys) <- selections xs ]
