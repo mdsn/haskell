d82a0e18c95212eef5aa22d4474c6a9c6454caa9
Mariano Casco <muy.poca.fe@gmail.com>
2013-06-01 20:47:21 -0300

    mirror and fmap implementation

--- tree.hs
+++ tree.hs
@@ -19,17 +19,27 @@ height = (flip g) 0
 leaves :: Tree a -> Int
 leaves = (flip g) 0
     where
-        g (Leaf _) i = i + 1
+        g (Leaf _) i     = i + 1
         g (Branch a b) i = (g a i) + (g b i)
 
 -- count non leaf nodes
 nodes :: Tree a -> Int
-nodes (Leaf _) = 0
+nodes (Leaf _)     = 0
 nodes (Branch a b) = 1 + (nodes a) + (nodes b)
 
 -- list leaves left to right
 walkover :: Tree a -> [Tree a]
 walkover = (flip g) []
     where
-        g (Leaf a) leaves = leaves ++ [Leaf a]
+        g (Leaf a) leaves     = leaves ++ [Leaf a]
         g (Branch a b) leaves = (g a leaves) ++ (g b [])
+
+-- return specular tree
+mirror :: Tree a -> Tree a
+mirror (Leaf a)     = Leaf a
+mirror (Branch a b) = Branch (mirror b) (mirror a)
+
+-- map
+instance Functor Tree where
+    fmap f (Leaf a)     = Leaf (f a)
+    fmap f (Branch a b) = Branch (fmap f a) (fmap f b)
