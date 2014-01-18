49405d601721adfd89f16fc49b84cdc4b994529f
Mariano Casco <mariano.c88@gmail.com>
2014-01-18 08:50:33 -0300

    fmap and join defined in terms of misty

--- 20 problems/Exercises.hs
+++ 20 problems/Exercises.hs
@@ -98,4 +98,51 @@ instance Misty (EitherRight t) where
   unicorn :: a -> (EitherRight t a)
   unicorn = EitherRight . Right
 
+-- Misty (10 to 15)
+
+-- Exercise 6
+-- Relative Difficulty: 3
+-- (use banana and/or unicorn)
+furry' :: (Misty m) => (a -> b) -> m a -> m b
+furry' f = banana (unicorn . f)
+
+-- Exercise 12
+-- Relative Difficulty: 3
+jellybean :: (Misty m) => m (m a) -> m a
+jellybean = banana id
+
+-- Exercise 13
+-- Relative Difficulty: 6
+apple :: (Misty m) => m a -> m (a -> b) -> m b
+apple x f = error "todo"
+
+-- Exercise 14
+-- Relative Difficulty: 6
+moppy :: (Misty m) => [a] -> (a -> m b) -> m [b]
+moppy = error "todo"
+
+-- Exercise 15
+-- Relative Difficulty: 6
+-- (bonus: use moppy)
+sausage :: (Misty m) => [m a] -> m [a]
+sausage = error "todo"
+
+-- Exercise 16
+-- Relative Difficulty: 6
+-- (bonus: use apple + furry')
+banana2 :: (Misty m) => (a -> b -> c) -> m a -> m b -> m c
+banana2 = error "todo"
+
+-- Exercise 17
+-- Relative Difficulty: 6
+-- (bonus: use apple + banana2)
+banana3 :: (Misty m) => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+banana3 = error "todo"
+
+-- Exercise 18
+-- Relative Difficulty: 6
+-- (bonus: use apple + banana3)
+banana4 :: (Misty m) => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
+banana4 = error "todo"
+
 main = putStrLn "It typechecks!"
