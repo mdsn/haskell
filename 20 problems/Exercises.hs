b00e9691967ca58c9563d528877e5f9023e7d717
Mariano Casco <mariano.c88@gmail.com>
2014-01-18 07:43:24 -0300

    added instances signatures

--- 20 problems/Exercises.hs
+++ 20 problems/Exercises.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 -- 20 intermediate haskell projects
 -- https://www.fpcomplete.com/user/DanBurton/20-intermediate-exercises
 
@@ -7,11 +8,13 @@ class Fluffy f where
 -- Exercise 1
 -- Relative Difficulty: 1
 instance Fluffy [] where
+  furry :: (a -> b) -> [a] -> [b]
   furry = map
 
 -- Exercise 2
 -- Relative Difficulty: 1
 instance Fluffy Maybe where
+  furry :: (a -> b) -> Maybe a -> Maybe b
   furry f x = case x of
       Just a -> Just (f a)
       Nothing -> Nothing
@@ -19,6 +22,7 @@ instance Fluffy Maybe where
 -- Exercise 3
 -- Relative Difficulty: 5
 instance Fluffy ((->) t) where
+  furry :: (a -> b) -> (t -> a) -> (t -> b)
   furry f g = f . g
 
 newtype EitherLeft b a = EitherLeft (Either a b)
@@ -27,14 +31,15 @@ newtype EitherRight a b = EitherRight (Either a b)
 -- Exercise 4
 -- Relative Difficulty: 5
 instance Fluffy (EitherLeft t) where
+  furry :: (a -> b) -> (EitherLeft t a) -> (EitherLeft t b)
   furry f (EitherLeft (Left a)) = EitherLeft $ Left (f a)
   furry f (EitherLeft (Right b)) = EitherLeft $ Right b
 
 -- Exercise 5
 -- Relative Difficulty: 5
 instance Fluffy (EitherRight t) where
+  furry :: (a -> b) -> (EitherRight t a) -> (EitherRight t b)
   furry f (EitherRight (Left a)) = EitherRight $ Left a
   furry f (EitherRight (Right b)) = EitherRight . Right $ f b
 
 main = putStrLn "It typechecks!"
-
