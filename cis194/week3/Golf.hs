fa02fb5b46c2fb8a91dcc83506cfafe1167505f4
Mariano Casco <mariano.c88@gmail.com>
2015-02-22 15:38:59 -0300

    cis 194, week 3: skips function

---
+++ cis194/week3/Golf.hs
@@ -0,0 +1,23 @@
+module Golf where
+
+skips :: [a] -> [[a]]
+skips [] = []
+skips xs = [every n xs | n <- [1..length xs]]
+  where
+    {- every -
+    Builds a list of (i, z) by zipping zs with an infinite list [1..n,1..n,...]
+    and filters on those indexed equal to n (the ones we have to keep), finally
+    getting rid of the indexes. -}
+    every :: Int -> [a] -> [a]
+    every n = map snd . filter ((== n) . fst) . zip (cycle [1..n])
+
+    {-
+    every :: Int -> [a] -> [a]
+    every _ [] = []
+    every n ys = every' 1 ys
+      where
+        every' _       [] = []
+        every' current (x:xs')
+          | n == current = x : every' 1 xs'
+          | otherwise    = every' (current + 1) xs'
+    -}
