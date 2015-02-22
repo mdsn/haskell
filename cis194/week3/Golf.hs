1ebfa9edb34ee0a83604f59a785b5379add866ab
Mariano Casco <mariano.c88@gmail.com>
2015-02-22 18:39:41 -0300

    cis-194, week 3: super fat histogram function

--- cis194/week3/Golf.hs
+++ cis194/week3/Golf.hs
@@ -1,5 +1,11 @@
 module Golf where
 
+import Control.Monad (liftM)
+import Data.Function (on)
+import Data.List (sortBy)
+
+{- Skips -}
+
 skips :: [a] -> [[a]]
 skips [] = []
 skips xs = [every n xs | n <- [1..length xs]]
@@ -22,8 +28,72 @@ skips xs = [every n xs | n <- [1..length xs]]
           | otherwise    = every' (current + 1) xs'
     -}
 
+{- Local maxima -}
+
 localMaxima :: [Integer] -> [Integer]
 localMaxima (x:y:z:xs) =
     if (y > x) && (y > z) then y : localMaxima (y:z:xs)
                           else localMaxima (y:z:xs)
 localMaxima _      = []
+
+{- Histogram -}
+
+type Histogram a = [Freq a]
+
+data Freq a = Freq { value :: a
+                   , freq  :: Int } deriving (Show, Eq)
+
+-- Comparison by value
+instance (Eq a, Ord a) => Ord (Freq a) where
+    compare a b = compare (value a) (value b)
+
+sortByFreq :: Histogram a -> Histogram a
+sortByFreq = sortBy (compare `on` freq)
+
+maxByFreq :: Histogram a -> Maybe (Freq a)
+maxByFreq [] = Nothing
+maxByFreq xs = let (m:_) = reverse (sortByFreq xs)
+                in Just m
+
+maxFreq :: Histogram a -> Maybe Int
+maxFreq xs = liftM freq (maxByFreq xs)
+
+freqOf :: (Eq a) => a -> Histogram a -> Int
+freqOf _ []                = 0
+freqOf a xs
+  | not (inHistogram a xs) = 0
+  | otherwise              = let (x:_) = filter ((== a) . value) xs
+                             in freq x
+
+inHistogram :: (Eq a) => a -> Histogram a -> Bool
+inHistogram _ []     = False
+inHistogram a (x:xs) = (a == value x) || inHistogram a xs
+
+incFreq :: Eq a => a -> Histogram a -> Histogram a
+incFreq a []     = [Freq a 1]
+incFreq a (x@(Freq v f):xs)
+  | a == v    = Freq v (f+1) : xs
+  | otherwise = x : incFreq a xs
+
+buildHistogram :: Eq a => [a] -> Histogram a
+buildHistogram = foldr incFreq []
+
+line :: Int -> Histogram Int -> String
+line 0 _  = ""
+line n xs = [f x | x <- [0..9]]
+  where
+    f n' | inHistogram n' xs && freqOf n' xs >= n = '*'
+         | otherwise                              = ' '
+
+histLines :: Histogram Int -> String
+histLines [] = ""
+histLines xs = case maxFreq xs of Just f  -> lines' f
+                                  Nothing -> ""
+  where
+    lines' :: Int -> String
+    lines' 0           = "==========\n0123456789\n"
+    lines' currentFreq = line currentFreq xs ++ "\n" ++ lines' (currentFreq-1)
+
+histogram :: [Int] -> String
+histogram [] = ""
+histogram ns = histLines (buildHistogram ns)
