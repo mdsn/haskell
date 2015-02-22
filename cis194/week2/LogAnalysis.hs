3e577f27b6271ae7d15069dfbd6aa035ad684f9d
Mariano Casco <mariano.c88@gmail.com>
2015-02-22 13:52:31 -0300

    cis 194 week 2, message tree

--- cis194/week2/LogAnalysis.hs
+++ cis194/week2/LogAnalysis.hs
@@ -38,3 +38,37 @@ parseMessage line = parseLine
 
 parse :: String -> [LogMessage]
 parse logs = map parseMessage (lines logs)
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert m@(LogMessage _ timestamp _) tree =
+    case tree of
+        Leaf -> Node Leaf m Leaf
+        Node left node right ->
+            case node of
+                (Unknown _) -> Node left node right
+                (LogMessage _ time _) -> if timestamp < time then Node (insert m left) node right
+                                                             else Node left node (insert m right)
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build ms = foldr insert Leaf ms
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node l x r) = left ++ [x] ++ right
+  where
+    left = inOrder l
+    right = inOrder r
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong ms = map message . filter severe . inOrder . build $ ms
+  where
+    severe :: LogMessage -> Bool
+    severe (LogMessage (Error severity) _ _) = severity >= 50
+    severe _                                 = False
+
+    message :: LogMessage -> String
+    message (LogMessage _ _ m) = m
+    message _                  = ""
