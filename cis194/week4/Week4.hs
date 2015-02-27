73c3a64fd4bd8a9a952baa87029110caebcabb1e
Mariano Casco <mariano.c88@gmail.com>
2015-02-26 22:38:25 -0300

    Wrong foldtree implementation

--- cis194/week4/Week4.hs
+++ cis194/week4/Week4.hs
@@ -1,5 +1,7 @@
 module Week4 where
 
+import Data.List (sort, partition)
+
 fun1 :: [Integer] -> Integer
 fun1 []      = 1
 fun1 (x:xs)
@@ -22,4 +24,33 @@ data Tree a = Leaf
             deriving (Show, Eq)
 
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree = foldr insert Leaf
+  where
+    insert :: a -> Tree a -> Tree a
+    insert x Leaf = Node 0 Leaf x Leaf
+    insert x (Node d l v r)
+      | leftHeight < rightHeight  = Node d (insert x l) v r
+      | leftHeight > rightHeight  = Node d l v r'
+      | leftHeight == rightHeight = Node (h+1) l v r'
+      where
+        leftHeight = height l
+        rightHeight = height r
+
+        r' = insert x r
+        h  = height r'
+
+        height :: Tree a -> Integer
+        height Leaf = 0
+        height (Node depth _ _ _) = depth
+
+printTree :: (Show a) => Tree a -> String
+printTree x = printTree' x 0
+  where
+    printTree' x depth = case x of
+      Node d l v r -> (replicate (2 * depth) ' ')
+                           ++ "D" ++ show d ++ " " ++ show v
+                           ++ "\n"
+                           ++ (printTree' l (depth + 1))
+                           ++ "\n"
+                           ++ (printTree' r (depth + 1))
+      Leaf -> (replicate (2 * depth) ' ') ++ "Leaf"
