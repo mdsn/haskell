7c15127ad9b19e78bfc5e1fb62e7329031ed7f88
Mariano Casco <mariano.c88@gmail.com>
2015-03-02 11:44:20 -0300

    cis194, week 5, add Expr class and instance

--- cis194/week5/Calc.hs
+++ cis194/week5/Calc.hs
@@ -3,6 +3,19 @@ module Calc where
 import ExprT
 import Parser
 
+class Expr a where
+    lit :: Integer -> a
+    mul :: a -> a -> a
+    add :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    mul = Mul
+    add = Add
+
+reify :: ExprT -> ExprT
+reify = id
+
 eval :: ExprT -> Integer
 eval e       = case e of
     (Add x y) -> recurse (+) x y
