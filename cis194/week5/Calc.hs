3362468ce438a2c11fd69202c7e6b25c0880bd3e
Mariano Casco <mariano.c88@gmail.com>
2015-03-02 15:47:33 -0300

    cis194, week 5, implement instance Expr Program

--- cis194/week5/Calc.hs
+++ cis194/week5/Calc.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Calc where
 
-import ExprT
+import qualified ExprT as E
+import StackVM
 import Parser
 
 class Expr a where
@@ -8,10 +12,15 @@ class Expr a where
     mul :: a -> a -> a
     add :: a -> a -> a
 
-instance Expr ExprT where
-    lit = Lit
-    mul = Mul
-    add = Add
+instance Expr Program where
+    lit n   = [PushI n]
+    mul x y = x ++ y ++ [Mul]
+    add x y = x ++ y ++ [Add]
+
+instance Expr E.ExprT where
+    lit = E.Lit
+    mul = E.Mul
+    add = E.Add
 
 instance Expr Integer where
     lit = id
@@ -30,24 +39,31 @@ instance Expr MinMax where
     mul = max
     add = min
 
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+    lit = Mod7 . (`mod` 7)
+    mul (Mod7 x) (Mod7 y) = lit $ x * y
+    add (Mod7 x) (Mod7 y) = lit $ x + y
+
 testExp :: Expr a => String -> Maybe a
 testExp = parseExp lit add mul
 
-reify :: ExprT -> ExprT
+reify :: E.ExprT -> E.ExprT
 reify = id
 
-eval :: ExprT -> Integer
+eval :: E.ExprT -> Integer
 eval e       = case e of
-    (Add x y) -> recurse (+) x y
-    (Mul x y) -> recurse (*) x y
-    (Lit n)   -> n
+    (E.Add x y) -> recurse (+) x y
+    (E.Mul x y) -> recurse (*) x y
+    (E.Lit n)   -> n
   where
-    recurse :: (Integer -> Integer -> Integer) -> ExprT -> ExprT -> Integer
+    recurse :: (Integer -> Integer -> Integer) -> E.ExprT -> E.ExprT -> Integer
     recurse f x y = f (eval x) (eval y)
 
 evalStr :: String -> Maybe Integer
 evalStr s =
-    case parseExp Lit Add Mul s of
+    case parseExp E.Lit E.Add E.Mul s of
         Just e -> Just $ eval e
         _      -> Nothing
 
